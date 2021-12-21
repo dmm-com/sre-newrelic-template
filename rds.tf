08cd75c5f300682ff73658a5649c8a8292134c8e
ono-hiroshi1 <ono-hiroshi1@dmm.com>
2021-12-21 15:06:39 +0900

    RDSの場合はRDS死活監視を行わないように変更した
    NetworkThroughputはリクエストが来ないとゼロになるため使っていない場合に死んでいるとご検知されてしまう

--- rds.tf
+++ rds.tf
@@ -1,29 +1,6 @@
 // --------------------
 // Expect Aurora
 // --------------------
-resource "newrelic_nrql_alert_condition" "rds_alive" {
-  policy_id      = newrelic_alert_policy.policy.id
-  type           = "static"
-  value_function = "single_value"
-
-  count                        = length(var.rds_alive_alerts)
-  name                         = var.rds_alive_alerts[count.index].name
-  violation_time_limit_seconds = 3600
-  expiration_duration          = 300
-  open_violation_on_expiration = true
-
-  nrql {
-    query             = "SELECT average(aws.rds.NetworkThroughput) FROM Metric WHERE aws.accountId IN (${var.aws_account_id}) AND tags.${var.rds_alive_alerts[count.index].tag_key} = '${var.rds_alive_alerts[count.index].tag_value}' FACET entityName"
-    evaluation_offset = 3
-  }
-  critical {
-    operator           = "equals"
-    threshold          = 0
-    threshold_duration = 60
-    threshold_occurrences = "ALL"
-  }
-}
-
 resource "newrelic_nrql_alert_condition" "rds_replica_lag" {
   policy_id      = newrelic_alert_policy.policy.id
   type           = "static"
