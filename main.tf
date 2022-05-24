04d5d16b9348fb26d7d1839feb378ac513f8c71f
niwano-satoru <niwano-satoru@dmm.com>
2022-05-24 17:04:14 +0900

    policyに関係するリソースブロックを別ファイルに切り出し

--- main.tf
+++ main.tf
@@ -26,23 +26,3 @@ provider "aws" {
 }
 
 data "aws_caller_identity" "self" {}
-
-// New Relic alert setting
-resource "newrelic_alert_policy" "policy" {
-  name = var.alert_policy_name
-}
-
-resource "newrelic_alert_channel" "channel" {
-  type = "slack"
-  name = var.alert_slack_channel.name
-
-  config {
-    url     = var.alert_slack_channel.url
-    channel = var.alert_slack_channel.channel
-  }
-}
-
-resource "newrelic_alert_policy_channel" "slack_channel" {
-  policy_id   = newrelic_alert_policy.policy.id
-  channel_ids = [newrelic_alert_channel.channel.id]
-}
