1eba920920b1580413ac74c3ab02d5125b2ea3ab
niwano-satoru <niwano-satoru@dmm.com>
2022-05-27 16:57:22 +0900

    Synthetics設定の設定値をローカル変数化

--- alert/src/environments/example/env.tf
+++ alert/src/environments/example/env.tf
@@ -8,4 +8,44 @@ locals {
   slack_mention = "" // Slack通知時のメンション先
 
   apm_app_name_prefix = "[sample]%" // NewRelic APMの監視対象とするappNameの接頭辞
+
+  newrelic_synthetics_ping = [
+    {
+      name                      = "[DMM] TopPage (ping)"
+      status                    = "ENABLED"
+      uri                       = "https://www.dmm.com/"
+      validation_string         = "" // レスポンスが正しいかチェックする時用のバリデーション文字列
+      verify_ssl                = true
+      bypass_head_request       = false // pingチェックのときデフォルトのHEADリクエストをスキップし、代わりにGETリクエストを使用する
+      treat_redirect_as_failure = false
+    },
+    {
+      name                      = "[FANZA] TopPage (ping)"
+      status                    = "ENABLED"
+      uri                       = "https://www.dmm.co.jp/"
+      validation_string         = "動画"
+      verify_ssl                = true
+      bypass_head_request       = false
+      treat_redirect_as_failure = false
+    }
+  ]
+
+  newrelic_synthetics_browser = [
+    {
+      name                = "[DMM] TopPage (browser)"
+      status              = "ENABLED"
+      uri                 = "https://www.dmm.com/"
+      validation_string   = "電子書籍"
+      verify_ssl          = true
+      bypass_head_request = false
+    },
+    {
+      name                = "[FANZA] TopPage (browser)"
+      status              = "ENABLED"
+      uri                 = "https://www.dmm.co.jp/"
+      validation_string   = "動画"
+      verify_ssl          = true
+      bypass_head_request = false
+    }
+  ]
 }
--- alert/src/environments/example/main.tf
+++ alert/src/environments/example/main.tf
@@ -98,49 +98,41 @@ module "alert_ec2" {
   slack_mention = local.slack_mention
 }
 
-module "alert_synthetics" {
+module "alert_synthetics_ping" {
   source = "../../modules/synthetics"
 
   policy_id     = module.alert_policy.newrelic_alert_policy_policy_id
   slack_mention = local.slack_mention
 
+  count = length(local.newrelic_synthetics_ping)
   newrelic_synthetics_ping = [
     {
-      name                      = "[DMM] TopPage (ping)"
-      status                    = "ENABLED"
-      uri                       = "https://www.dmm.com/"
-      validation_string         = "" // レスポンスが正しいかチェックする時用のバリデーション文字列
-      verify_ssl                = true
-      bypass_head_request       = false // pingチェックのときデフォルトのHEADリクエストをスキップし、代わりにGETリクエストを使用する
-      treat_redirect_as_failure = false
-    },
-    {
-      name                      = "[FANZA] TopPage (ping)"
-      status                    = "ENABLED"
-      uri                       = "https://www.dmm.co.jp/"
-      validation_string         = "動画"
-      verify_ssl                = true
-      bypass_head_request       = false
-      treat_redirect_as_failure = false
+      name                      = local.newrelic_synthetics_ping[count.index].name
+      status                    = local.newrelic_synthetics_ping[count.index].status
+      uri                       = local.newrelic_synthetics_ping[count.index].uri
+      validation_string         = local.newrelic_synthetics_ping[count.index].validation_string
+      verify_ssl                = local.newrelic_synthetics_ping[count.index].verify_ssl
+      bypass_head_request       = local.newrelic_synthetics_ping[count.index].bypass_head_request
+      treat_redirect_as_failure = local.newrelic_synthetics_ping[count.index].treat_redirect_as_failure
     }
   ]
+}
 
+module "alert_synthetics_browser" {
+  source = "../../modules/synthetics"
+
+  policy_id     = module.alert_policy.newrelic_alert_policy_policy_id
+  slack_mention = local.slack_mention
+
+  count = length(local.newrelic_synthetics_browser)
   newrelic_synthetics_browser = [
     {
-      name                = "[DMM] TopPage (browser)"
-      status              = "ENABLED"
-      uri                 = "https://www.dmm.com/"
-      validation_string   = "電子書籍"
-      verify_ssl          = true
-      bypass_head_request = false
-    },
-    {
-      name                = "[FANZA] TopPage (browser)"
-      status              = "ENABLED"
-      uri                 = "https://www.dmm.co.jp/"
-      validation_string   = "動画"
-      verify_ssl          = true
-      bypass_head_request = false
+      name                      = local.newrelic_synthetics_browser[count.index].name
+      status                    = local.newrelic_synthetics_browser[count.index].status
+      uri                       = local.newrelic_synthetics_browser[count.index].uri
+      validation_string         = local.newrelic_synthetics_browser[count.index].validation_string
+      verify_ssl                = local.newrelic_synthetics_browser[count.index].verify_ssl
+      bypass_head_request       = local.newrelic_synthetics_browser[count.index].bypass_head_request
     }
   ]
 }
