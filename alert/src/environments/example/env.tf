07895b7987f360c687b7ee272632808d78a13a06
niwano-satoru <niwano-satoru@dmm.com>
2022-05-27 16:52:34 +0900

    サンプル設定を追加

--- alert/src/environments/example/env.tf
+++ alert/src/environments/example/env.tf
@@ -1,11 +1,11 @@
 locals {
-  alert_policy_name = "" // アラートポリシー名
+  alert_policy_name = "SampleAlert" // アラートポリシー名
   alert_slack_channel = {
-    name    = "" // NewRelicチャンネル設定名
+    name    = "SampleAlertSlack" // NewRelicチャンネル設定名
     url     = "" // Slack Webhook URL
     channel = "" // Slackチャンネル名
   }
   slack_mention = "" // Slack通知時のメンション先
 
-  apm_app_name_prefix = "" // NewRelic APMの監視対象とするappNameの接頭辞
+  apm_app_name_prefix = "[sample]%" // NewRelic APMの監視対象とするappNameの接頭辞
 }
