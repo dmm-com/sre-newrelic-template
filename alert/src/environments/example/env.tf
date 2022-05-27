a5466e4110167aab5be8a505e75170cfc6f38069
niwano-satoru <niwano-satoru@dmm.com>
2022-05-27 09:36:53 +0900

    環境で変わるlocal変数を別ファイルに切り出し

---
+++ alert/src/environments/example/env.tf
@@ -0,0 +1,11 @@
+locals {
+  alert_policy_name = ""
+  alert_slack_channel = {
+    name    = ""
+    url     = "" // slack hook url
+    channel = "" // Team Channel用
+  }
+  slack_mention = ""
+
+  apm_app_name_prefix = ""
+}
