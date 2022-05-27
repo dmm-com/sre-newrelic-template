33eea740919485d4e03227011411c8cd6cc45650
niwano-satoru <niwano-satoru@dmm.com>
2022-05-27 17:13:37 +0900

    設定の説明を追加

--- alert/src/environments/example/env.tf
+++ alert/src/environments/example/env.tf
@@ -11,13 +11,13 @@ locals {
 
   newrelic_synthetics_ping = [
     {
-      name                      = "[DMM] TopPage (ping)"
-      status                    = "ENABLED"
-      uri                       = "https://www.dmm.com/"
-      validation_string         = "" // レスポンスが正しいかチェックする時用のバリデーション文字列
-      verify_ssl                = true
-      bypass_head_request       = false // pingチェックのときデフォルトのHEADリクエストをスキップし、代わりにGETリクエストを使用する
-      treat_redirect_as_failure = false
+      name                      = "[DMM] TopPage (ping)" // Synthetics監視名
+      status                    = "ENABLED" // 設定の有効／無効
+      uri                       = "https://www.dmm.com/" // 監視対象URL
+      validation_string         = "" // 応答で検証する文字列
+      verify_ssl                = true // SSL証明書検証の有無
+      bypass_head_request       = false // HEAD リクエストをスキップしGETリクエストするかどうか
+      treat_redirect_as_failure = false // リダイレクトされた場合に監視を失敗とするかどうか
     },
     {
       name                      = "[FANZA] TopPage (ping)"
