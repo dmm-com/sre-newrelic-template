57dc4e661eef4356e4d50d83c66c070976f3739b
niwano-satoru <niwano-satoru@dmm.com>
2022-05-27 16:43:14 +0900

    不要空白削除

--- alert/src/environments/example/locals.tf
+++ alert/src/environments/example/locals.tf
@@ -1,6 +1,6 @@
 locals {
   aws_region = "ap-northeast-1"
 
-  nr_account_id  =  // NewRelicアカウントID, 数値型
-  nr_api_key     = "" // Type:USERのAPIキー
+  nr_account_id =  // NewRelicアカウントID, 数値型
+  nr_api_key    = "" // Type:USERのAPIキー
 }
