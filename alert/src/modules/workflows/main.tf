59ce9a6dab0f78ba8f43163b3d9426f5c91a7440
niwano-satoru <niwano-satoru@dmm.com>
2022-09-30 14:55:10 +0900

    メール通知のチャンネルを作成

--- alert/src/modules/workflows/main.tf
+++ alert/src/modules/workflows/main.tf
@@ -9,3 +9,19 @@ resource "newrelic_notification_destination" "email" {
     value = var.email_notification_destination
   }
 }
+
+resource "newrelic_notification_channel" "email" {
+  account_id = var.nr_account_id
+
+  name = "email-example"
+  type = "EMAIL"
+
+  destination_id = newrelic_notification_destination.email.id
+
+  product = "IINT"
+
+  property {
+    key   = "subject"
+    value = "{{ issueTitle }}"
+  }
+}
