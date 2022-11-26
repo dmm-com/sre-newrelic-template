46f9e82a2ba01eee24a9ff2a293ae5f927841f2f
niwano-satoru <niwano-satoru@dmm.com>
2022-11-26 13:08:07 +0900

    変数を変更

--- dashboard/src/modules/aws_newrelic_charge/main.tf
+++ dashboard/src/modules/aws_newrelic_charge/main.tf
@@ -2,7 +2,7 @@
 //
 resource "newrelic_one_dashboard_json" "aws_newrelic_charge" {
    json = templatefile("${path.module}/tftpl/aws_newrelic_charge.json.tftpl", {
-      account_id = var.nr_account_id,
+      nr_account_id = var.nr_account_id,
       exchange_rate = var.exchange_rate,
       applications = [
         {
