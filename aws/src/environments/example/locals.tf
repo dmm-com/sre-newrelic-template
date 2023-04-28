3bc1a89e15e7a000545c3424bb634d119755eeb9
niwano-satoru <niwano-satoru@dmm.com>
2023-04-28 15:24:20 +0900

    tfsec実行

--- aws/src/environments/example/locals.tf
+++ aws/src/environments/example/locals.tf
@@ -1,5 +1,5 @@
 locals {
-  nr_account_id  = 1234567                                    // NewRelicアカウントID, 数値型
-  nr_api_key     = "NRAK-xxxxxxxxxxxxxxxxxxxxxxxxxxx"         // Type:USERのAPIキー
-  nr_license_key = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxFFFFNRAL" // Type:Licenseキー
+  nr_account_id  = 1234567                                    # NewRelicアカウントID, 数値型
+  nr_api_key     = "NRAK-xxxxxxxxxxxxxxxxxxxxxxxxxxx"         # Type:USERのAPIキー
+  nr_license_key = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxFFFFNRAL" # Type:Licenseキー
 }
