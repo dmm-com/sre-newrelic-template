96543569297b89328a89fb792fe3bd83edb0544c
niwano-satoru <niwano-satoru@dmm.com>
2024-04-20 17:02:14 +0900

    productionとexampleの内容を同じにしproductionを削除

--- aws/src/environments/example/locals.tf
+++
@@ -1,5 +0,0 @@
-locals {
-  nr_account_id  = 1234567                                    # NewRelicアカウントID, 数値型
-  nr_api_key     = "NRAK-xxxxxxxxxxxxxxxxxxxxxxxxxxx"         # Type:USERのAPIキー
-  nr_license_key = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxFFFFNRAL" # Type:Licenseキー
-}
